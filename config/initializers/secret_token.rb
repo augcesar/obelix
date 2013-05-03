7f335bfc501bad6d89a6379569a8903eb51f009b
Augusto César <augusto@ubuntu.(none)>
2013-05-03 18:45:11 -0300

    Iniciando projeto

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Obelix::Application.config.secret_token = 'd29222a3964270bf1320309aa7391123458c33ca73c62bb7714be2b673d79172313cad1aa1030933973fe8268962e4c9d534b3f5508d5113a0ce0ce83c1ed105'
